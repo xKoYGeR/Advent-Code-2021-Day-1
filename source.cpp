12ca6c72baa50353d8f3b75c06931e30a2391ed6
xKoYGeR <95703948+xKoYGeR@users.noreply.github.com>
2021-12-08 08:32:27 +0100

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -6,30 +6,48 @@
 
 int main()
 {
+    /* LOADING DATA */
     std::fstream dataFile;
     dataFile.open("NUMBERS.txt", std::ios::in);
-    std::vector <int> vecStringNumbers;
+    std::vector <int> vecNumbers;
     if (dataFile.good())
     {
         std::string strNumLine;
         while (!dataFile.eof())
         {
             std::getline(dataFile, strNumLine);
-            vecStringNumbers.push_back(atoi(strNumLine.c_str()));
+            vecNumbers.push_back(atoi(strNumLine.c_str()));
         }
         dataFile.close();
     }
     else std::cout << "Error!" << std::endl;
 
-    if (vecStringNumbers.empty())
+    if (vecNumbers.empty())
         return 0;
 
+    /* PART I */
     int numMeasurement = 0;
-    for (int i = 1; i < vecStringNumbers.size(); i++)
-        if (vecStringNumbers[i - 1] < vecStringNumbers[i])
+    for (int i = 1; i < vecNumbers.size(); i++)
+        if (vecNumbers[i - 1] < vecNumbers[i])
             numMeasurement++;
 
+    /* PART II */
+    int numMeasurement2 = 0;
+    for (int i = 1; i < vecNumbers.size() - 2; i++)
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            sum1 += vecNumbers[i - 1 + j];
+            sum2 += vecNumbers[i + j];
+        }
+        if (sum1 < sum2)
+            numMeasurement2++;
+    }
+
     std::cout << numMeasurement << std::endl;
+    std::cout << numMeasurement2 << std::endl;
     std::cin.get();
     return 0;
 }
